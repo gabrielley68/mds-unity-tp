b44c64fe765a6344258d503a6d7c22bc05e80000
Lythom <sam_1er2002@yahoo.fr>
2020-03-16 14:24:49 +0100

    Add namespace and improve SpritesheetAnimator

---
+++ Assets/Part2-physics/Scripts/MoveCharacter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+
+namespace Part2
+{
+    [RequireComponent(typeof(SpriteRenderer), typeof(SpritesheetAnimator))]
+    public class MoveCharacter : MonoBehaviour
+    {
+        private const string ROLL = "roll";
+
+        [Tooltip("Speed in Unit per second")] public float speed = 5f;
+
+        private SpriteRenderer spriteRenderer;
+        private SpritesheetAnimator animator;
+
+        // COOLDOWNS
+        [Tooltip("Cooldown of a roll in seconds")]
+        public float rollCooldownDuration = 1;
+
+        private float rollCooldown = 0;
+
+        void Start()
+        {
+            this.transform.position = new Vector3(2, 2, 0);
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            animator = GetComponent<SpritesheetAnimator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 move = Vector3.zero;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                move += Vector3.up;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                move += Vector3.down;
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                move += Vector3.left;
+                spriteRenderer.flipX = true;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                move += Vector3.right;
+                spriteRenderer.flipX = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && rollCooldown <= 0)
+            {
+                animator.Play(Anims.Roll);
+                rollCooldown = rollCooldownDuration;
+            }
+
+            if (animator.CurrentAnimation.name != Anims.Roll || animator.LoopCount >= 1)
+            {
+                if (move.magnitude > 0)
+                {
+                    animator.Play(Anims.Run);
+                }
+                else
+                {
+                    animator.Play(Anims.Iddle);
+                }
+            }
+
+            this.transform.position = this.transform.position + speed * Time.deltaTime * move.normalized;
+            rollCooldown -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Part2-physics/Scripts/SpritesheetAnimator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Part2
+{
+    public enum Anims
+    {
+        Iddle,
+        Run,
+        Roll
+    }
+
+    [Serializable]
+    public struct AnimDefinition
+    {
+        public Anims name;
+        public Sprite[] frames;
+    }
+
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpritesheetAnimator : MonoBehaviour
+    {
+
+        [Tooltip("animation speed in images per seconds")]
+        public float animationSpeed = 25;
+
+        public AnimDefinition[] animations;
+
+        // accessors (getters)
+        public AnimDefinition CurrentAnimation => currentAnimation;
+        public int LoopCount => loopCount;
+        public float animationFrameDuration => 1f / animationSpeed;
+
+        // private properties
+        private SpriteRenderer spriteRenderer;
+        private AnimDefinition currentAnimation;
+
+        private int currentFrameIndex = 0;
+
+        // time in seconds before next frame is displayed
+        private float nextFrameCoolDown;
+
+        // number of time the current animation have been played
+        private int loopCount = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            nextFrameCoolDown = animationFrameDuration;
+            currentAnimation = animations[0];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GetCurrentAnimationFrames().Length == 0) return;
+
+            if (nextFrameCoolDown <= 0)
+            {
+                AnimateNextFrame();
+            }
+
+            nextFrameCoolDown -= Time.deltaTime;
+        }
+
+        public void AnimateNextFrame()
+        {
+            currentFrameIndex = (currentFrameIndex + 1) % GetCurrentAnimationFrames().Length;
+            spriteRenderer.sprite = GetCurrentAnimationFrames()[currentFrameIndex];
+            nextFrameCoolDown += animationFrameDuration;
+            if (currentFrameIndex == 0) loopCount++;
+        }
+
+        // TODO: permettre un callback lorsque l'animation est terminée
+        public void Play(Anims nextAnimation)
+        {
+            if (currentAnimation.name == nextAnimation) return;
+            currentAnimation = animations.First(a => a.name == nextAnimation);
+            currentFrameIndex = 0;
+            loopCount = 0;
+        }
+
+        private Sprite[] GetCurrentAnimationFrames()
+        {
+            return currentAnimation.frames;
+        }
+    }
+}
